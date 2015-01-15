f5a00e7d5593e1c3ac5d80a21765b65c8832356b
migilman3297 <mgilman3297@gmail.com>
2015-01-15 18:30:07 -0500

    niggga

--- geneticalgo.py
+++ geneticalgo.py
@@ -1,4 +1,3 @@
-import sys
 # To change this license heade , choose License Headers in Project Properties.
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
@@ -13,15 +12,16 @@ import random
 
 class Letter:
     
-    x, y, char, genome = 0, 0, '', {'speed' : 0, 'health' : 0, 'decay' : 0 }
+    x, y, char, genome = 0, 0, '', {'speed' : 0.0, 'health' : 0, 'decay' : 0 }
     
     def __init__(self):
         self.x = random.randint(1,75)
         self.y = random.randint(1, 30)
-        self.char = chr(random.randint(97, 123))
-        self.speed = random.random() + 1
-        self.health = random.randint(1, 5)
-        self.decay = random.randint(0, 1)
+        self.char = chr(random.randint(97, 122))
+        self.genome['speed'] = 1 + random.random()
+        self.genome['health'] = random.randint(1, 5)
+        self.genome['decay'] = random.randint(0, 1)  
+         
         
     def update_display(self):
         y_disp = self.y * '\n'
@@ -29,21 +29,46 @@ class Letter:
         y_remainder = (30 - self.y) * '\n'
         letter = y_disp + x_disp + self.char + y_remainder
         print letter
-    
+        
     def random_walk(self):
         x_mov = random.choice([-1, 0, 1])
         y_mov = random.choice([-1, 0, 1])
         self.x += x_mov 
         self.y += y_mov
-
+    
     def run(self):
-        while True:
+        for p in range(4):
+            rate = .2 * self.genome['speed']
             self.update_display()
             self.random_walk()
-            time.sleep(.1 * self.genome['speed'])
+            time.sleep(rate)
             os.system('clear')
-  
- 
+                   
+        
+
+class Poison(Letter):
+    
+    speed = 0
+    
+    def __init__(self):
+        Letter.__init__(self)
+        
+    
+    def collide(self):
+        star = '*'
+        y_disp = self.y * '\n'
+        x_disp = self.x * ' '
+        y_remainder = (30 - self.y) * '\n'
+        stellar = y_disp + x_disp + star + y_remainder
+        print Letter.y, self.y, Letter.x, self.x
+        print stellar
+        
+    def random_walk(self):
+        return Letter.random_walk(self)
+    
+        
+
 letter = Letter()
-print letter.genome['speed']
-letter.run()
+poison = Poison()
+#print letter.genome['speed']
+#letter.run()
